900fac85529be5eb214c5913f7bab8b2c34ed450
Ashraful Islam <4134005+ashraful-islam@users.noreply.github.com>
2019-03-02 12:58:38 +0100

    add: implementation in js

---
+++ index.js
@@ -0,0 +1,31 @@
+const SIZE_MAP = {
+  '32bit': {
+          PRIME: 16777619,
+          OFFSET_BASIS: 2166136261,
+  },
+  '64bit': {
+          PRIME: 1099511628211,
+          OFFSET_BASIS: 14695981039346656037,
+  },
+  '128bit': {
+          PRIME: 309485009821345068724781371,
+          OFFSET_BASIS: 144066263297769815596495629667062367629
+  }
+};
+
+function fnv1a(str, size) {
+  let PRIME, OFFSET_BASIS;
+  if (SIZE_MAP.hasOwnProperty(size)) {
+      PRIME = SIZE_MAP[size].PRIME;
+      OFFSET_BASIS = SIZE_MAP[size].OFFSET_BASIS;
+  } else {
+      throw new Error(`Invalid or unsupported size: ${string(size)} provided.`);
+  }
+  let hash = OFFSET_BASIS;
+  for ( let i = 0; i < str.length; ++i ) {
+      hash = hash ^ str.charAt(i);
+      hash = hash * PRIME;
+  }
+
+  return hash;
+}
